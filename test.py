ad2a9de47c7a60e20bc97008951a91adb9b55ca4
bot11 <ramakrishnadeepak@gmail.com>
2014-06-25 20:53:21 +0530

    --

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
 import ConfigParser
 
-filename='test.ini'
-parser=ConfigParser.SafeConfigParser()
+filename = 'test.ini'
+parser = ConfigParser.SafeConfigParser()
 parser.read([filename])
-try:
-	for section in parser.sections():
-		try:
-			host = section
-			username=parser.get(section,'username')
-			password=parser.get(section,'password')
-			print "host= %s, username = %s, password = %s" %(host, username, password)
-		except ValueError, err:
-			print "Error while parsing", err
-except DuplicateSectionError, err:
-	print "Duplicate Error", err
-	
+
+for section in parser.sections():
+    try:
+        host = section
+        username = parser.get(section, 'username')
+        password = parser.get(section, 'password')
+        print "host= %s, username = %s, password = %s" % (host, username, password)
+    except ValueError, err:
+        print "Error while parsing", err
